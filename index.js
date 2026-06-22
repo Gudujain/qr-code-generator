26b16d970490b49d12254938276d7ee05a268087
Tanisha Jain <jaintanu0987@gmail.com>
2026-06-22 10:51:07 +0530

    Complete QR code project

--- index.js
+++ index.js
@@ -3,3 +3,31 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import inquirer from 'inquirer';
+
+import qr from 'qr-image';
+
+import fs from 'fs';
+
+inquirer
+    .prompt(
+        [
+            {"message":"type in your url",
+                name:'url'}
+        ]
+    )
+
+    .then((answers) => {
+        var ans=answers.url;
+        var qr_svg=qr.image(ans);
+        qr_svg.pipe(fs.createWriteStream("qr_img.png"))
+
+    })
+
+
+
+
+
+
+
